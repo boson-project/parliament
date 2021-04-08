0bf9d52df4139294596156f989c4dbfa94b4bca3
Matej Vasek <mvasek@redhat.com>
2021-04-08 20:54:22 +0200

    src:use class not dict (#9)
    Signed-off-by: Matej Vasek <mvasek@redhat.com>

--- tests/func.py
+++ tests/func.py
@@ -1,11 +1,14 @@
-def main(context):
+from parliament.server import Context
+
+
+def main(context: Context):
     """
     Provides a function fixture to be used in tests
     """
-    if "cloud_event" in context:
-        event = context["cloud_event"]
+    if context.cloud_event is not None:
+        event = context.cloud_event
         return event.data["message"], 200
     else:
-        data = context["request"].get_json(silent=True)
+        data = context.request.get_json(silent=True)
         print(data)
         return data["message"]
