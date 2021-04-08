e6d9a505685a023cdc89542ac6116cc040c2232f
Matej Vasek <mvasek@redhat.com>
2021-04-08 20:31:53 +0200

    use class not dict
    Signed-off-by: Matej Vasek <mvasek@redhat.com>

--- parliament/__init__.py
+++ parliament/__init__.py
@@ -1,5 +1,8 @@
 # __init__.py
+
 from . import server
 
+Context = server.Context
+
 __version__ = "0.0.5"
-__all__ = [server]
+__all__ = [server, Context]
--- parliament/server.py
+++ parliament/server.py
@@ -1,8 +1,30 @@
 import os
 import sys
 
-from flask import Flask, request
-from cloudevents.http import from_http
+from flask import Flask, request, Request
+from cloudevents.http import from_http, CloudEvent
+
+
+class Context(object):
+    """
+    Class holding invocation context.
+
+    ...
+
+    Attributes
+    ----------
+    request:
+        Flask HTTP request
+    cloud_event:
+        CloudEvent if any
+    """
+    request: Request
+    cloud_event: CloudEvent
+    __slots__ = ['request', 'cloud_event']
+
+    def __init__(self, req: Request):
+        self.request = req
+        self.cloud_event = None
 
 
 def load(path):
@@ -23,12 +45,10 @@ def create(func):
 
     @app.route("/", methods=["POST"])
     def handle_post():
-        context = {
-            'request': request
-        }
+        context = Context(request)
         try:
-            context['cloud_event'] = from_http(
-                request.headers, request.get_data())
+            context.cloud_event = from_http(request.headers,
+                                            request.get_data())
         except Exception:
             app.logger.warning('No CloudEvent available')
 
@@ -39,9 +59,7 @@ def create(func):
 
     @app.route("/", methods=["GET"])
     def handle_get():
-        context = {
-            'request': request
-        }
+        context = Context(request)
         return func.main(context)
 
     @app.route("/health/liveness")
