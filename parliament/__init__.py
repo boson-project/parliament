6c4db6a3ed864497156952ae67df7a0562d00d0e
Matej Vasek <mvasek@redhat.com>
2021-04-08 21:18:11 +0200

    refactor: move Context
    Signed-off-by: Matej Vasek <mvasek@redhat.com>

--- parliament/__init__.py
+++ parliament/__init__.py
@@ -1,8 +1,31 @@
 # __init__.py
 
-from . import server
 
-Context = server.Context
+from flask import Request
+from cloudevents.http import CloudEvent
+
+
+class Context(object):
+    """
+    Class holding invocation context.
+
+    ...
+
+    Attributes
+    ----------
+    request:
+        Flask HTTP request
+    cloud_event:
+        CloudEvent if any
+    """
+    request: Request
+    cloud_event: CloudEvent
+    __slots__ = ['request', 'cloud_event']
+
+    def __init__(self, req: Request):
+        self.request = req
+        self.cloud_event = None
+
 
 __version__ = "0.0.5"
-__all__ = [server, Context]
+__all__ = ["server", "Context"]
