789c6654c548d73408845b42c2f25f9649d7866a
Lance Ball <lball@redhat.com>
2021-04-12 09:05:07 -0400

    fix: use version.py to satisfy release-please (#16)
    Signed-off-by: Lance Ball <lball@redhat.com>

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
 from setuptools import setup
 import pathlib
-import parliament
+import os
+
 
 HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
+package_root = os.path.abspath(os.path.dirname(__file__))
+
+version = {}
+with open(os.path.join(package_root, "version.py")) as fp:
+    exec(fp.read(), version)
+version = version["__version__"]
+
 setup(
     name='parliament-functions',
     packages=['parliament'],
-    version=parliament.__version__,
+    version=version,
     license='MIT',
     description='A framework for invoking functions over HTTP',
     long_description=README,
