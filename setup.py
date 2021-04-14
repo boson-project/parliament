54c87e2271b463fc62625702c31cf28cb1a52f65
Lance Ball <lball@redhat.com>
2021-04-14 09:02:21 -0400

    fix: fix path to version.py (#19)
    Signed-off-by: Lance Ball <lball@redhat.com>

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 package_root = os.path.abspath(os.path.dirname(__file__))
 
 version = {}
-with open(os.path.join(package_root, "version.py")) as fp:
+with open(os.path.join(package_root, "parliament", "version.py")) as fp:
     exec(fp.read(), version)
 version = version["__version__"]
 
